67e6036c564f4e2eb9acf650acf5c33813af3003
Agnethe Søraa <agnethes0raa@gmail.com>
2014-06-04 17:38:24 -0700

    make serve_image return an image

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
+from PIL import Image
+import StringIO
 app = Flask(__name__)
 
 
@@ -7,9 +9,16 @@ def hello_world():
     return render_template('index.html')
 
 
-"""@app.route('/<username>')
-def hello_world2(username):
-    return 'Hello %s' % username"""
+@app.route('/<width>x<height>')
+@app.route('/<width>X<height>')
+def serve_image(width, height):
+    stringfile = StringIO.StringIO()
+    im = Image.open("static/images/annie.jpg")
+    im.save(stringfile, 'JPEG')
+    response = make_response(stringfile.getvalue())
+    response.headers["Content-Type"] = "image/jpeg"
+    return response
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
