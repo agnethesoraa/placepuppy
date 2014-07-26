f368b5d862e91dd07e95a76beb963f58867db5f6
Agnethe Søraa <agnethes0raa@gmail.com>
2014-07-26 19:25:06 +0200

    add support to choose imagetype

--- image_helpers.py
+++ image_helpers.py
@@ -4,8 +4,44 @@ import StringIO
 
 def create_image(width, height):
     stringfile = StringIO.StringIO()
-    im = Image.open("static/images/annie.jpg")
+    im = Image.open(choose_image(width, height))
     im.thumbnail((int(width), int(height)), Image.ANTIALIAS)
     im = im.crop((0, 0, int(width), int(height)))
     im.save(stringfile, 'JPEG')
     return stringfile
+
+
+def is_portrait(width, height):
+    return height > width
+
+
+def is_square(width, height):
+    return width == height
+
+
+def is_landscape(width, height):
+    return height < width
+
+
+def list_squares():
+    return ["puppies/squares/S001.jpg", "puppies/squares/S002.jpg",
+            "puppies/squares/S003.jpg"]
+
+
+def list_portraits():
+    return ["puppies/portraits/P001.jpg", "puppies/portraits/P002.jpg",
+            "puppies/portraits/P003.jpg"]
+
+
+def list_landscapes():
+    return ["puppies/landscapes/L001.jpg", "puppies/landscapes/L002.jpg",
+            "puppies/landscapes/L003.jpg"]
+
+
+def choose_image(width, height):
+    if is_portrait(width, height):
+        return list_portraits()[0]
+    elif is_square(width, height):
+        return list_squares()[0]
+    elif is_landscape(width, height):
+        return list_landscapes()[0]
